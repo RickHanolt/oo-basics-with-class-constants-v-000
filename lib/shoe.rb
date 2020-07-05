e166b1b88cc0ab81cffc24645e91904fc1ffb041
Richard Hanolt <rhanolt@gmail.com>
2020-07-05 10:56:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
+class Shoe
+  attr_accessor :brand
+  attr_reader :genre
 
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def genre=(genre)
+    GENRES << genre
+  end
+
+end
